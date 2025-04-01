3cf52c3bd090a2950822ac73daf18850c8cbd927
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2025-04-01 15:24:12 -0400

    Third time's a charm?

--- server/server.ts
+++ server/server.ts
@@ -1,12 +1,12 @@
 import express from "express";
 import apiRouter from "./routes/api/api.index";
 import authRouter from "./routes/auth/auth.index";
+import config from "./config/config";
 
 import { configurePassport } from "./middlewares/passport.mw";
 import path from "path";
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
@@ -22,6 +22,8 @@ app.get("*", (_, res) => {
 	res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
+// Start the server and bind to the correct port
+const PORT = process.env.PORT || config.app.port;
 app.listen(PORT, () => {
-	console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}~`);
 });
