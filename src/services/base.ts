b83f26e85b95a8c7c217ad131a1cec60de9813fb
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2025-04-01 15:33:27 -0400

    Try four!

--- src/services/base.ts
+++ src/services/base.ts
@@ -1,18 +1,18 @@
-import storage from '../utils/storage';
+import storage from "../utils/storage";
 
 //This screws your prod, make sure you have the prod info here lol
 const makeFetch = async (url: string, info: RequestInit) => {
-  const response = await fetch(`${import.meta.env.VITE_ROOT_URL}${url}`, info);
+	const response = await fetch(url, info);
 
-  if (!response.ok) {
-    const errorData = await response.json();
-    console.error(`ERROR`, errorData);
-    if (response.status === 403) {
-      alert(`${errorData.message}`);
-    }
-    throw new Error(errorData.message || 'Something went wrong');
-  }
-  return response;
+	if (!response.ok) {
+		const errorData = await response.json();
+		console.error(`ERROR`, errorData);
+		if (response.status === 403) {
+			alert(`${errorData.message}`);
+		}
+		throw new Error(errorData.message || "Something went wrong");
+	}
+	return response;
 };
 
 /**
@@ -24,24 +24,24 @@ const makeFetch = async (url: string, info: RequestInit) => {
  * @returns whatever we expect from the backend or an error if something went wrong
  */
 const json = async (url: string, method: string, body: object = {}) => {
-  const TOKEN = storage.getToken();
-  const headers: HeadersInit = {
-    'Content-Type': 'application/json',
-  };
+	const TOKEN = storage.getToken();
+	const headers: HeadersInit = {
+		"Content-Type": "application/json",
+	};
 
-  //If there's a token in storage, then we add it here to show that the user is either authorized by the token or is spoofing the token which will casue an error on the backend if so.
-  if (TOKEN) {
-    headers['Authorization'] = `Bearer ${TOKEN}`;
-  }
+	//If there's a token in storage, then we add it here to show that the user is either authorized by the token or is spoofing the token which will casue an error on the backend if so.
+	if (TOKEN) {
+		headers["Authorization"] = `Bearer ${TOKEN}`;
+	}
 
-  const data: RequestInit = {
-    method,
-    headers,
-    body: method !== 'GET' ? JSON.stringify(body) : undefined,
-  };
+	const data: RequestInit = {
+		method,
+		headers,
+		body: method !== "GET" ? JSON.stringify(body) : undefined,
+	};
 
-  const fetchResponse = await makeFetch(url, data);
-  return await fetchResponse.json();
+	const fetchResponse = await makeFetch(url, data);
+	return await fetchResponse.json();
 };
 /**
  * Catchers for the different types of requests, formatting the data and passing into out base json function what it needs to understand what type of request we want.
@@ -49,22 +49,22 @@ const json = async (url: string, method: string, body: object = {}) => {
  * @returns whatever we expect from the backend or an error if one was thrown
  */
 const get = (url: string) => {
-  return json(url, 'GET');
+	return json(url, "GET");
 };
 const post = (url: string, payload: object) => {
-  console.log(`PAYLOAD in base service:`, payload);
-  return json(url, 'POST', payload);
+	console.log(`PAYLOAD in base service:`, payload);
+	return json(url, "POST", payload);
 };
 const put = (url: string, payload: object) => {
-  return json(url, 'PUT', payload);
+	return json(url, "PUT", payload);
 };
 const destroy = (url: string) => {
-  return json(url, 'DELETE');
+	return json(url, "DELETE");
 };
 
 export default {
-  get,
-  post,
-  put,
-  destroy,
+	get,
+	post,
+	put,
+	destroy,
 };
