493720aa9e0e564260949ed4e2278ba57875bd8b
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2025-04-01 16:00:00 -0400

    Cleanup to ping backend not localhost

--- src/services/fetchAugments.ts
+++ src/services/fetchAugments.ts
@@ -1,5 +1,5 @@
 export const fetchAugments = async (): Promise<any[]> => {
-	const res = await fetch("http://localhost:3001/api/augments/");
+	const res = await fetch("/api/augments/");
 	const augments = await res.json();
 	return augments;
 };
