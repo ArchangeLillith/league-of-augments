59c722e63ab12020bd2b0169146e4b642e90b1e6
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2025-04-16 11:21:51 -0400

    CSS changes and testing on prod DB

--- src/services/fetchAugments.ts
+++ src/services/fetchAugments.ts
@@ -1,6 +1,7 @@
 export const fetchAugments = async (tags = false): Promise<any[]> => {
 	if (tags) {
 		const res = await fetch("/api/augments/includeTags");
+		console.log("Augments and tags in frontend:", res);
 		const augments = await res.json();
 		return augments;
 	} else {
