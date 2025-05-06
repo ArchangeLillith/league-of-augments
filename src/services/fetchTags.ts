91919a6dbe9e54908df54f114eeeeb365d1c3d50
ArchangeLillith <90073620+ArchangeLillith@users.noreply.github.com>
2025-05-06 09:53:50 -0400

    Fix to tags fetch

--- src/services/fetchTags.ts
+++ src/services/fetchTags.ts
@@ -1,17 +1,11 @@
 import baseService from "./base";
 
-export const fetchTags = async (
-): Promise<any[]> => {
-  try {
-    const builds = await baseService.get("/api/tags");
-    for (let build of builds) {
-      if (build.augments[0].augment_id === null) {
-        build.augments = [];
-      }
-    }
-    return builds;
-  } catch (error) {
-    console.error(`ERROR in auth.ts in services:`, error);
-    throw error;
-  }
+export const fetchTags = async (): Promise<any[]> => {
+	try {
+		const tags = await baseService.get("/api/tags");
+		return tags;
+	} catch (error) {
+		console.error(`ERROR in auth.ts in services:`, error);
+		throw error;
+	}
 };
